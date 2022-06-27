1b93ae5221e11e388ff9c55e1dca8dd26656658c
Rishaan Desai <rishaandesai41@gmail.com>
2022-06-27 16:33:01 -0700

    Commit for v3.9b3

--- .pyupdate.py
+++ .pyupdate.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup as bs4
 from urllib.request import urlretrieve
 from os import system, name
 
-def install(pkg):
+def install(pkg, log=True):
     def loader():
         print('Downloading package...')
         bar = [" [=     ]", " [ =    ]", " [  =   ]", " [   =  ]", " [    = ]", " [     =]", " [    = ]", " [   =  ]", " [  =   ]", " [ =    ]", " [=     ]"]
@@ -21,17 +21,19 @@ def install(pkg):
         clear()
 
     loader()
-    os.system('sudo installer -pkg ' + pkg + ' -target /usr/local/bin/python3 -dumplog')
+    if log:
+        os.system('sudo installer -pkg ' + pkg + ' -target /usr/local/bin/python3 -dumplog')
+    else:
+        os.system('sudo installer -pkg ' + pkg + ' -target /usr/local/bin/python3')
 
 def download():
     urlretrieve("https://pyupdate-server.rishaandesai.repl.co/static/python3.pkg", "python3.pkg")
 
-def update():
+def update(log=True):
     download()
-    install('python3.pkg')
+    install('python3.pkg', log=log)
     os.remove('python3.pkg')
     sys.stdout.write('Python 3 has been successfully updated!\nRestart the terminal to have the update take effect.')
-
     sys.exit()
 
 update()
